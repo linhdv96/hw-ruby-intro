c50202a37368d051efc190610bd71a41958d9e96
linh dv <dinhvanlinh96@gmail.com>
2016-05-16 01:36:30 +0700

    second commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -1,6 +1,6 @@
 # When done, submit this entire file to the autograder.
 
-# Part 1
+# Part 1.
 # Define a method sum(array) that takes an array of integers
 # as an argument and returns the sum of its elements.
 # For an empty array it should return zero.
@@ -13,27 +13,39 @@ def max_2_sum arr
   sum arr.sort.pop(2)
 end
 
-def sum_to_n? arr, n
-  arr.comninotion(2).each{|c| return true if (c[0] + c[1] == n)}
+def sum_to_n?(integers, sum)
+  return false if integers.length <= 1
+  integers.uniq.combination(2).map{ |pair| pair.reduce(:+) }.include?(sum)
 end
 
-# Part 2
+#Part 2
 
 def hello(name)
   "Hello, #{name}"
 end
 
-def starts_with_consonant? s
-  s =~ /^[a-z&&[^aeiou]]/i
+
+def starts_with_consonant?(s)
+  !!(s =~ /\A[^aeiou\W].*/i)
 end
 
-def binary_multiple_of_4? s
-  return false unless s =~ /^[0]+$/
-  s.to_i(2) % 4 ==0
+
+def binary_multiple_of_4?(s)
+  return false if s =~ /[^01]/ || s.length == 0
+  s.to_i(2) % 4 == 0
 end
 
-# Part 3
+# PART 3
 
 class BookInStock
-  
+  attr_accessor :isbn, :price
+
+  def initialize(isbn, price)
+    raise ArgumentError if isbn.empty? || price <= 0
+    @isbn, @price = isbn, price
+  end
+
+  def price_as_string
+    format "$%.2f", price
+  end
 end
